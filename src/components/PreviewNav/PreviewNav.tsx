d6cfad2f04811c8b2b4f20c777196a38027c2fc0
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-08-15 18:16:25 +0300

    LS-implemented-copy-profile-link-to-clipboard

--- src/components/PreviewNav/PreviewNav.tsx
+++ src/components/PreviewNav/PreviewNav.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './PreviewNav.scss';
 
-function PreviewNav() {
+interface Props {
+    handleCopyLinkToClipboard: () => void
+}
+
+function PreviewNav({handleCopyLinkToClipboard}: Props) {
     return (
         <div className="d-flex align-items-center justify-content-between preview-nav">
             <Link id="link" to="/*">
                 Back to Editor
             </Link>
-            <button>
+            <button onClick={handleCopyLinkToClipboard}>
                 Share Link
             </button>
         </div>
--- src/pages/profile/ProfilePreview/ProfilePreview.tsx
+++ src/pages/profile/ProfilePreview/ProfilePreview.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useLocation} from "react-router-dom";
 import './ProfilePreview.scss';
+import {HiOutlineLink} from 'react-icons/hi';
 import ProfileSummary from '../../../components/ProfileSummary/ProfileSummary';
 import PreviewNav from "../../../components/PreviewNav/PreviewNav";
+import ProfileSubmit from "../../../components/ProfileSubmit";
 
 function ProfilePreview() {
+
+    const [isCopied, setCopied] = useState<boolean>(false);
+
+    const location = useLocation();
+
+    const locationPrefix = 'http://localhost:3000';
+
+    const handleCopyLinkToClipboard = async () => {
+        try {
+            let profileUrl = locationPrefix + location.pathname;
+
+            await navigator.clipboard.writeText(profileUrl);
+
+            return setCopied(true);
+        } catch (err) {
+            console.error('Failed to copy URL:', err);
+        }
+    }
+
+    const handleCloseCopiedAlert = () => {
+        setCopied(false);
+    }
 
     const isMobile = window.innerWidth <= 395;
 
     return (
         <div className="main-container-preview">
-            <PreviewNav />
-            {!isMobile && <div className="preview-header" />}
-            <ProfileSummary
-                mock={false}
+            <PreviewNav
+                handleCopyLinkToClipboard={handleCopyLinkToClipboard}
+            />
+            {!isMobile &&
+                <div className="preview-header" />
+            }
+            <ProfileSummary mock={false} />
+            <ProfileSubmit
+                message={'The link has been copied to your clipboard!'}
+                open={isCopied}
+                icon={<HiOutlineLink />}
+                handleCloseSubmitAlert={handleCloseCopiedAlert}
             />
         </div>
     )
